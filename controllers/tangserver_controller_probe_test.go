952d9994b16b26ab13a47110b174ffc9e5243b9b
Sergio Arroutbi Braojos <sarroutb@redhat.com>
2021-07-26 10:43:25 +0200

    Extend unit tested code coverage

---
+++ controllers/tangserver_controller_probe_test.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	daemonsv1alpha1 "github.com/sarroutbi/tang-operator/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("TangServer controller probe", func() {
+
+	// Define utility constants for object names and testing timeouts/durations and intervals.
+	const (
+		TangserverName = "test-tangserver-probe"
+		// TODO: test why it can not be tested in non default namespace
+		TangserverNamespace        = "default"
+		TangserverResourceVersion  = "1"
+		TangServerTestHealthScript = "/tmp/test-health-script"
+	)
+
+	Context("When Creating TangServer", func() {
+		It("Should be created with default script value", func() {
+			By("By creating a new TangServer with empty image specs")
+			ctx := context.Background()
+			tangServer := &daemonsv1alpha1.TangServer{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      TangserverName,
+					Namespace: TangserverNamespace,
+				},
+				Spec: daemonsv1alpha1.TangServerSpec{
+					Replicas: 1,
+				},
+			}
+			Expect(k8sClient.Create(ctx, tangServer)).Should(Succeed())
+			Expect(getProbe(tangServer).Handler.Exec.Command[0], DEFAULT_DEPLOYMENT_HEALTH_CHECK)
+			k8sClient.Delete(ctx, tangServer)
+		})
+		It("Should be created with default script value", func() {
+			By("By creating a new TangServer with empty image specs")
+			ctx := context.Background()
+			tangServer := &daemonsv1alpha1.TangServer{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      TangserverName,
+					Namespace: TangserverNamespace,
+				},
+				Spec: daemonsv1alpha1.TangServerSpec{
+					Replicas:     1,
+					HealthScript: TangServerTestHealthScript,
+				},
+			}
+			Expect(k8sClient.Create(ctx, tangServer)).Should(Succeed())
+			Expect(getProbe(tangServer).Handler.Exec.Command[0], DEFAULT_DEPLOYMENT_HEALTH_CHECK)
+			k8sClient.Delete(ctx, tangServer)
+		})
+	})
+})
