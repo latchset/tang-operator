bbd89742e85ec45b03d0d5c33e348d5418b5f08e
Sergio Arroutbi Braojos <sarroutb@redhat.com>
2021-08-06 14:59:35 +0200

    Fix path

--- .github/workflows/scripts/retrieve_operator_sdk.sh
+++ .github/workflows/scripts/retrieve_operator_sdk.sh
@@ -5,11 +5,12 @@ OPERATOR_SDK_DEFAULT_RELEASE_VERSION="v1.8.2"
 
 OPERATOR_SDK_RELEASE_VERSION="${1}"
 
-if [ -z "$OPERATOR_SDK_RELEASE_VERSION" ]; then
+if [ -z "${OPERATOR_SDK_RELEASE_VERSION}" ]; then
   echo "INFO: operator-sdk release version is not set. Defaulting to ${OPERATOR_SDK_DEFAULT_RELEASE_VERSION}"
   OPERATOR_SDK_RELEASE_VERSION="${OPERATOR_SDK_DEFAULT_RELEASE_VERSION}"
 fi
 
-curl -L -o /operator-sdk "https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_RELEASE_VERSION}/operator-sdk_linux_amd64"
+echo "PWD:$(pwd)"
 
-chmod +x /operator-sdk
+curl -L -o operator-sdk "https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_RELEASE_VERSION}/operator-sdk_linux_amd64"
+chmod +x operator-sdk
