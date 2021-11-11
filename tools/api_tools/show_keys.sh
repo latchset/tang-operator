227b0b7ecda6da8544a5ff877ba926bd273c8096
Sergio Arroutbi Braojos <sarroutb@redhat.com>
2021-11-11 17:56:11 +0100

    Include script to show signing key from service adv

---
+++ tools/api_tools/show_keys.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -e
+
+namespace=""
+using_minikube=""
+
+usage() {
+  echo
+  echo "Usage:"
+  echo
+  echo "$1 -n namespace [-c k8s_client] [-m (using minikube)]"
+  echo
+  exit "$2"
+}
+
+while getopts "n:c:hm" arg
+do
+case "${arg}" in
+  n) namespace=${OPTARG}
+  ;;
+  c) oc_client=${OPTARG}
+  ;;
+  m) using_minikube="yes"
+  ;;
+  h) usage "$0" 0
+  ;;
+  *) usage "$0" 1
+  ;;
+esac
+done
+
+test -z "${namespace}" && namespace="default"
+test -z "${oc_client}" && oc_client="oc"
+
+getAdvUrl() {
+  if [ "${using_minikube}" != "yes" ];
+  then
+      "${oc_client}" -n "${namespace}" get tangservers.daemons.redhat.com  -o json | jq '.items[0].status.serviceExternalUrl'
+  else
+      port=$("${oc_client}" -n "${namespace}" get service -o json | jq '.items[0].spec.ports[0].nodePort')
+      echo "http://$(minikube ip):${port}/adv"
+  fi
+}
+
+adv_url=$(getAdvUrl)
+adv=$(wget -O - ${adv_url} -o /dev/null)
+
+dumpWithHash() {
+    local adv="$1"
+    local hash="$2"
+    jose fmt --json "${adv}" -g payload -y -o- | jose jwk use -i- -r -u verify -o- \
+        | jose jwk thp -i- -a "${hash}"    
+}
+
+echo "===ADV (URL:${adv_url})==="
+echo "${adv}"
+echo "===/ADV (URL:${adv_url})==="
+echo "===FORMATTED ADV (URL:${adv_url})==="
+echo "${adv}" | jq
+echo "===/FORMATTED ADV (URL:${adv_url})==="
+echo
+echo "===JOSE PAYLOAD==="
+payload=$(jose fmt --json "${adv}" -g payload -y -o-)
+echo "${payload}"
+echo "===/JOSE PAYLOAD==="
+echo "===FORMATTED JOSE PAYLOAD==="
+echo "${payload}" | jq
+echo "===/FORMATTED JOSE PAYLOAD==="
+echo
+echo "===JOSE VERIFY==="
+verify=$(echo "${payload}" | jose jwk use -i- -r -u verify -o-)
+echo "${payload}"
+echo "===/JOSE VERIFY==="
+echo "===FORMATTED JOSE VERIFY==="
+echo "${verify}" | jq
+echo "===/FORMATTED JOSE VERIFY==="
+echo
+echo "===SIGNING KEY==="
+echo "SHA1:$(dumpWithHash "${adv}" "S1")"
+echo "SHA256:$(dumpWithHash "${adv}" "S256")"
+echo "===/SIGNING KEY==="
