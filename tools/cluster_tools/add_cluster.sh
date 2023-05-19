8ae30e2c82a8e45e48c12f2ed2bb602fb38aeabe
Sergio Arroutbi <sarroutb@redhat.com>
2023-05-19 10:49:19 +0200

    Fix shellcheck issues for add_cluster.sh script
    Resolves: #30
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tools/cluster_tools/add_cluster.sh
+++ tools/cluster_tools/add_cluster.sh
@@ -8,8 +8,7 @@ alias cp='cp -rfv'
 
 function usage() {
   echo "$1 -f config-file [-c context] [-n namespace]"
-  #exit $2
-  return $2
+  return "$2"
 }
 
 while getopts "f:c:n:h" arg
@@ -24,7 +23,9 @@ do
     n) NAMESPACE=${OPTARG}
        echo "NAMESPACE=${NAMESPACE}"
        ;;
-    h) usage $0 0
+    h) usage "$0" 0
+       ;;
+    *) usage "$0" 1
        ;;
   esac
 done
@@ -35,10 +36,9 @@ then
 fi
 
 CONFIG_FILE_PATH=$(readlink -f "${CONFIG_FILE}")
-if [ -f ${HOME}/.kube/config.onlyMinikube ];
+if [ -f "${HOME}"/.kube/config.onlyMinikube ];
 then
-  echo "cp ${HOME}/.kube/config.onlyMinikube ${HOME}/.kube/config"
-  cp ${HOME}/.kube/config.onlyMinikube ${HOME}/.kube/config
+  cp "${HOME}"/.kube/config.onlyMinikube "${HOME}"/.kube/config -v
 fi
 
 export KUBECONFIG=${HOME}/.kube/config:${CONFIG_FILE_PATH}
@@ -49,9 +49,9 @@ kubectl config view --raw > /tmp/kubeconfig
 echo "cp /tmp/kubeconfig ~/.kube/config"
 cp /tmp/kubeconfig ~/.kube/config
 
-if [ ! -z "${CONTEXT}" ];
+if [ -n "${CONTEXT}" ];
 then
-  if [ ! -z "${NAMESPACE}" ];
+  if [ -n "${NAMESPACE}" ];
   then
     echo "kubectl config set-context ${CONTEXT} --namespace=${NAMESPACE}"
   fi
