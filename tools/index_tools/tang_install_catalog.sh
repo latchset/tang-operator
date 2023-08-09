1f9ed7e7ef52accb571ba56f1695822161752ea5
Sergio Arroutbi <sarroutb@redhat.com>
2023-08-09 08:04:44 +0200

    Include list option to show just install info (#149)
    Resolves: #148
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tools/index_tools/tang_install_catalog.sh
+++ tools/index_tools/tang_install_catalog.sh
@@ -1,4 +1,48 @@
 #!/bin/bash
+#
+# Copyright 2023 sarroutb@redhat.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom
+# the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+list_only=""
+
+function usage() {
+    echo
+    echo "Usage:"
+    echo
+    echo "$1 [-h] [-l]"
+    echo
+    exit "$2"
+}
+
+while getopts "hl" arg
+do
+  case "${arg}" in
+    l) list_only="true"
+       ;;
+    h) usage "$0" 0
+       ;;
+    *) usage "$0" 1
+       ;;
+  esac
+done
+
 echo "-------------------------------------"
 echo "oc project openshift-operators"
 oc project openshift-operators
@@ -6,9 +50,12 @@ echo "-------------------------------------"
 echo "oc status"
 oc status
 echo "-------------------------------------"
-sleep 1
-echo "oc create -f tang_catalog_source.yaml"
-oc create -f tang_catalog_source.yaml
+if [ -z "${list_only}" ];
+then
+    sleep 1
+    echo "oc create -f tang_catalog_source.yaml"
+    oc create -f tang_catalog_source.yaml
+fi
 echo "-------------------------------------"
 sleep 1
 echo "oc get catsrc -nopenshift-marketplace"
@@ -18,9 +65,12 @@ sleep 1
 echo "oc get pods -nopenshift-marketplace"
 oc get pods -nopenshift-marketplace
 echo "-------------------------------------"
-sleep 1
-echo "oc create -f tang_subscription.yaml"
-oc create -f tang_subscription.yaml
+if [ -z "${list_only}" ];
+then
+    sleep 1
+    echo "oc create -f tang_subscription.yaml"
+    oc create -f tang_subscription.yaml
+fi
 echo "-------------------------------------"
 echo "oc project"
 oc project
