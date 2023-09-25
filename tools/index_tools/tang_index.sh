244df1f7aa22a1ab1f49e7e4743fb858f5e5ec3f
Sergio Arroutbi <sarroutb@redhat.com>
2023-09-25 10:13:06 +0200

    Homogeneize licensing (Apache 2.0 to all)
    Resolves: #150
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tools/index_tools/tang_index.sh
+++ tools/index_tools/tang_index.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-# Parameters:
-# 1 - $1 image: quay.io/<repository>
-# 2 - $2 is for release, format like 0.0.25-1
-# Example:
 #
-# ./tang_index.sh quay.io/sec-eng-special/tang-operator-bundle:1.0.0-0 1.0.0-0
+#   Copyright [2023] [sarroutb (at) redhat.com]
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   Parameters:
+#   1 - $1 image: quay.io/<repository>
+#   2 - $2 is for release, format like 0.0.25-1
+#   Example:
+#   
+#   ./tang_index.sh quay.io/sec-eng-special/tang-operator-bundle:1.0.0-0 1.0.0-0
 #
 #
 CONTAINER_MGR='docker'
