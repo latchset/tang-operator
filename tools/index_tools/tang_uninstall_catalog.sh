85b3beb123c66f32e5bc9e8166d9f0d1a503d4f6
Sergio Arroutbi <sarroutb@redhat.com>
2023-09-28 17:33:56 +0200

    Delete CSV on uninstall (#162)
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- tools/index_tools/tang_uninstall_catalog.sh
+++ tools/index_tools/tang_uninstall_catalog.sh
@@ -17,3 +17,6 @@
 
 oc delete -f tang_subscription.yaml 
 oc delete -f tang_catalog_source.yaml 
+oc get csv -nopenshift-operators
+csv=$(oc get csv -n openshift-operators | grep -v NAME | head | awk '{print $1}')
+test -z "${csv}" || oc delete csv "${csv}" -n openshift-operators
